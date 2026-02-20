b5e63f9f1864cabf0d63626a4c458f5f198663c6
Chizycodes <okwudirejoy@gmail.com>
2026-02-20 20:54:17 +0100

    Backend: setup app with spring initializer

---
+++ backend/src/test/java/com/med_triage_api/MedTriageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med_triage_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedTriageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
