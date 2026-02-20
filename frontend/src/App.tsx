792ccb61282446fe2cf562be1ad4b107c7b3b989
Chizycodes <okwudirejoy@gmail.com>
2026-02-20 21:32:34 +0100

    frontend: route setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Questionnaire from "./pages/Questionnaire";
+import Recommendation from "./pages/Recommendation";
+import Confirmation from "./pages/Confirmation";
+
 function App() {
 	return (
-		<>
-			<div className="">App</div>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/questionnaire" element={<Questionnaire />} />
+				<Route path="/recommendation" element={<Recommendation />} />
+				<Route path="/confirmed" element={<Confirmation />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
